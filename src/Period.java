c89d4aa1d0599d3e87ed04286bead223bffb6ac8
kelly-david <davidkelly.tlf@gmail.com>
2018-02-07 13:27:56 +0000

    Instantiate Period objects

--- src/Period.java
+++ src/Period.java
@@ -1,4 +1,17 @@
 public class Period {
-    public Period() {
+    int startHour;
+    int endHour;
+
+    public Period(int start, int end) {
+        this.startHour = start;
+        this.endHour = end;
+    }
+
+    int duration() {
+        return 0;
+    };
+
+    boolean overlaps() {
+        return true;
     }
 }
--- src/RateTest.java
+++ src/RateTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Test;
@@ -10,46 +11,44 @@ import java.util.ArrayList;
  */
 
 public class RateTest {
+    ArrayList<Period> discountPeriods;
+    ArrayList<Period> normalPeriods;
+
+    @Before
+    public void instantiatePeriods() {
+        discountPeriods = new ArrayList<>();
+        discountPeriods.add(new Period(6,7));
+        normalPeriods = new ArrayList<>();
+        normalPeriods.add(new Period(2,5));
+    }
 
     @org.junit.Test(expected = IllegalArgumentException.class)
     public void normalRateLessThan0() throws Exception {
-        ArrayList<Period> discountPeriods = new ArrayList();
-        ArrayList<Period> normalPeriods = new ArrayList();
         Rate rt = new Rate(Rate.CarParkKind.VISITOR, BigDecimal.valueOf(-1), BigDecimal.valueOf(2), discountPeriods, normalPeriods);
     }
 
     @org.junit.Test
     public void normalRateGreaterThanZero() {
-        ArrayList<Period> discountPeriods = new ArrayList();
-        ArrayList<Period> normalPeriods = new ArrayList();
-        Rate rt = new Rate(Rate.CarParkKind.STUDENT, BigDecimal.valueOf(2), BigDecimal.valueOf(0), discountPeriods, normalPeriods);
+        Rate rt = new Rate(Rate.CarParkKind.STUDENT, BigDecimal.valueOf(2), BigDecimal.valueOf(1), discountPeriods, normalPeriods);
     }
 
     @org.junit.Test
     public void normalRateEqualOne() {
-        ArrayList<Period> discountPeriods = new ArrayList();
-        ArrayList<Period> normalPeriods = new ArrayList();
-        Rate rt = new Rate(Rate.CarParkKind.STUDENT, BigDecimal.valueOf(1), BigDecimal.valueOf(0), discountPeriods, normalPeriods);
+        Rate rt = new Rate(Rate.CarParkKind.STUDENT, BigDecimal.valueOf(1), BigDecimal.valueOf(1), discountPeriods, normalPeriods);
     }
 
     @org.junit.Test(expected = IllegalArgumentException.class)
     public void normalRateEqualZero() throws Exception {
-        ArrayList<Period> discountPeriods = new ArrayList();
-        ArrayList<Period> normalPeriods = new ArrayList();
         Rate rt = new Rate(Rate.CarParkKind.STUDENT, BigDecimal.valueOf(0), BigDecimal.valueOf(0), discountPeriods, normalPeriods);
     }
 
     @org.junit.Test(expected = IllegalArgumentException.class)
     public void normalRateLessThanDiscountRate() throws Exception {
-        ArrayList<Period> discountPeriods = new ArrayList();
-        ArrayList<Period> normalPeriods = new ArrayList();
         Rate rt = new Rate(Rate.CarParkKind.STUDENT, BigDecimal.valueOf(5), BigDecimal.valueOf(7), discountPeriods, normalPeriods);
     }
 
     @org.junit.Test
     public void normalRateGreaterThanDiscountRate() {
-        ArrayList<Period> discountPeriods = new ArrayList();
-        ArrayList<Period> normalPeriods = new ArrayList();
         Rate rt = new Rate(Rate.CarParkKind.STUDENT, BigDecimal.valueOf(5), BigDecimal.valueOf(4), discountPeriods, normalPeriods);
     }
 }
