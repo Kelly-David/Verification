fcf1b50f8067ce934429566ef3667dd3941a8a3f
kelly-david <davidkelly.tlf@gmail.com>
2018-02-13 11:44:47 +0000

    Moving to task2

--- src/CarParkKind.java
+++ src/CarParkKind.java
@@ -1,8 +1,6 @@
 /**
- * DUMMY CLASS
- * Created by davidkelly on 07/02/2018.
+ * Created by CM on 01/02/2018.
  */
-
-public enum CarParkKind {STAFF, STUDENT, MANAGEMENT, VISITOR};
-
-
+public enum CarParkKind {
+    STAFF, STUDENT, MANAGEMENT, VISITOR
+}
